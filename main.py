5fc5d8a31631b95e1961a5c8d5497b17a5beb077
jeevashhhkjs <jeevasdckap@gmail.com>
2023-10-04 10:34:26 +0530

    if else finished

---
+++ main.py
@@ -0,0 +1,34 @@
+""" Variables and Datatypes """
+# Variables
+a = 10
+b = 20
+
+# Datatypes
+"""
+"jee" => string
+20 => number
+4.5 => float
+"""
+
+# casting => it change the data type
+"""
+a = "10"
+b = int(a) # it return string to integer 
+print(type(b))
+"""
+
+
+# input method => get input from user it return string datatype only
+"""
+a = input("Put your name")
+print(a)
+b = input("put your age :")
+print(type(int(b)))
+"""
+
+# if else
+if ("name" == "name"):
+    print("true")
+else:
+    print("false")
+
